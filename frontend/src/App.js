b3ca0a6c34127c0bd4463be50ea4847f9bdc9b95
Glace97 <glacier.ali@gmail.com>
2021-05-17 23:06:35 +0200

    start of front tournament page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Tournament  from './components/Tournament';
 
 function App() {
   return (
     <div className="App">
+      <Tournament />
     </div>
   );
 }
---
+++ frontend/src/components/Tournament.jsx
@@ -0,0 +1,18 @@
+/*{
+    id: "325425",
+    name: "Audi JulCup",
+    start_date: "2019-01-13",
+    end_date: "2019-01-18"
+    },
+*/
+
+import React from 'react';
+
+export default function Tournament() {
+    return(
+        <>
+            <h1> Tournament Tracker </h1>
+            <button> Hello </button>
+        </>
+    );
+}
