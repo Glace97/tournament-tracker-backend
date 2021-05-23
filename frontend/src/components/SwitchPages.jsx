9bf952739001ad15741528f7ef23a50545944628
Glace97 <glacier.ali@gmail.com>
2021-05-23 17:34:50 +0200

    tournament buttons renders new page + correct url

--- frontend/src/components/SwitchPages.jsx
+++ frontend/src/components/SwitchPages.jsx
@@ -1,19 +1,24 @@
 //switch between pages component
 import React from "react";
-import {
-    Switch,
-    Route,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 //components
 import TournamenPage from './TournamentPage';
 import SchedulePage from "./SchedulePage";
 
+//redux
+import { useSelector } from 'react-redux';
 
-export default function SwitchPages() {
-    return (
+
+export default function SwitchPages() {    
+    
+   //get currently selected tournament for specific url path
+   const tournamendID = useSelector((state) => state.tournament.selectedTournament); 
+   const scheduleUrl = `/tournaments/${tournamendID}/schedule`
+
+   return (
         <Switch>
-            <Route path="/tournaments/1/schedule">   {/*HELT hårdkodad path, uppdatera så ID:et är dynamiskt*/}
+            <Route path={scheduleUrl}>   
                 <SchedulePage />
             </Route >
             <Route path="/tournaments">
--- frontend/src/components/Tournament.jsx
+++ frontend/src/components/Tournament.jsx
@@ -1,4 +1,3 @@
-
 //react imports
 import React from 'react';
 import { useHistory } from 'react-router-dom';
@@ -12,7 +11,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { tournament } from '../redux/ducks/tournament'
 
 //TO DO:
-// 1. ---ONCLICK FUNCTIONIONALITY-- DONE
 // 2. STYLE PAPER
 // 3. MOVE STYLE ATTRIBUTES TO USESTYLES HOOK
 
@@ -21,21 +19,16 @@ export default function Tournament({ name, id }) {
     //custom style obj for tournamentsheet
     const classes = useStyles();
 
-    //get state from reducer, kanske inte behövs sen framöver?
-    const selectedTournament = useSelector((state) => state.tournament.selectedTournament); 
-
-    //call any functions our duck has, ie dispatch an actions of (argument) type
     const dispatch = useDispatch();
     
     const history = useHistory(); 
 
+    
+    //update current tournament on selection and change browser url
     const handleClick = () => { 
-        //update current tournament obj state
         dispatch(tournament(id));
-        console.log("current tournament: " + selectedTournament) //TA BORT
-        history.push(`/${id}/schedule`);    
+        history.push(`/tournaments/${id}/schedule`);    
     };
-
 
     return (
         <div className={classes.root}>
