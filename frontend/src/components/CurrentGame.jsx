3c7398d6e967790d13eb18a4b6c26611afd808c2
Glace97 <glacier.ali@gmail.com>
2021-06-01 10:46:57 +0200

    minor fix current gamecard

--- frontend/src/components/CurrentGame.jsx
+++ frontend/src/components/CurrentGame.jsx
@@ -17,17 +17,18 @@ export default function CurrentGame({ league, player1, player2, court }) {
         <div className={classes.root}>
             <Card elevation={12} style={{ backgroundColor: '#6A9225', padding: '20px', fontWeight: '600', color: '#f7f7ed', textAlign: 'center' }}>
                 <Grid container spacing={12}>
+                <Grid item xs={12}>
+                        <Typography variant="h5"> {league} </Typography>
+                    </Grid>
+
                     <Grid item xs={4}> {player1} </Grid>
                     <Grid item xs={4}>
                         <Typography> vs </Typography>
                     </Grid>
                     <Grid item xs={4}> {player2}  </Grid>
-                    <Grid item xs={12}>
-                        <Typography> {league} </Typography>
-                    </Grid>
 
                     <Grid item xs={12}>
-                        <Typography> {court} </Typography>
+                        <Typography variant="h5"> On: {court} </Typography>
                     </Grid>
                 </Grid>
             </Card>
